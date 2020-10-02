b64ce022c25e34f0fe68b54520c4c94b8dadbe62
Larry Castillo <larrycastillo2009@gmail.com>
2020-10-01 20:29:22 -0500

    passing methods as props and binding

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
 
     }
 
-    switchNameHandler = () =>{
+    switchNameHandler = (newName) =>{
         // console.log('Was clicked!');
         // DONT DO THIS this.state.persons[0].name = 'Hairy Larry';
         this.setState({persons: [
-                {name: 'Hairy Larry', age: 30},
+                {name: newName, age: 30},
                 {name: 'Max', age: 28},
                 {name: 'Manu', age: 28}
             ]})
@@ -27,10 +27,17 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I am a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
+                <button onClick={()=> this.switchNameHandler('Hairy Larry')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this,'Harry Larry')} >My Hobbies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[0].age}/>
 
             </div>
         );
