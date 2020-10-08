33cea2e225fb6cbe3ee51c50d8d2973e0539c57d
Larry Castillo <larrycastillo2009@gmail.com>
2020-10-07 19:48:42 -0500

    organizing conditional logic in the javascript way

--- src/App.js
+++ src/App.js
@@ -14,39 +14,63 @@ class App extends Component {
 
     }
 
-    switchNameHandler = (newName) =>{
+    switchNameHandler = (newName) => {
         // console.log('Was clicked!');
         // DONT DO THIS this.state.persons[0].name = 'Hairy Larry';
-        this.setState({persons: [
+        this.setState({
+            persons: [
                 {name: newName, age: 30},
                 {name: 'Max', age: 28},
                 {name: 'Manu', age: 28}
-            ]})
+            ]
+        })
     }
 
-    nameChangedHandler =(event)=>{
-        this.setState({persons: [
-                {name: 'Larry', age: 30},
-                {name: event.target.value, age: 28},
-                {name: 'Manu', age: 28}
-            ]}
-
+    nameChangedHandler = (event) => {
+        this.setState({
+                persons: [
+                    {name: 'Larry', age: 30},
+                    {name: event.target.value, age: 28},
+                    {name: 'Manu', age: 28}
+                ]
+            }
         )
     }
 
     togglePersonsHandler = () => {
-        const doesShow =this.state.showPersons;
+        const doesShow = this.state.showPersons;
         this.setState({showPersons: !doesShow});
     }
 
     render() {
         const style = {
-            backgroundColor:'white',
-            font:'inherit',
+            backgroundColor: 'white',
+            font: 'inherit',
             border: '1p x solid blue',
             padding: '8px',
             cursor: 'pointer'
         };
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Harry Larry')}
+                        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
 
         return (
             <div className="App">
@@ -54,24 +78,9 @@ class App extends Component {
                 <p>This is really working!</p>
                 <button
                     style={style}
-                    onClick={()=> this.togglePersonsHandler()}>Toggle Persons</button>
-                { this.state.showPersons ?
-                    <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}/>
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this,'Harry Larry')}
-                        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}/>
-                </div> : null
-                }
-
-
+                    onClick={() => this.togglePersonsHandler()}>Toggle Persons
+                </button>
+                {persons}
             </div>
         );
         // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Does this work now?'))
