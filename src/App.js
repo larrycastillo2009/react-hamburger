f35ef7d2b0907a90111803cbbe4f7e09bda8bc10
Larry Castillo <larrycastillo2009@gmail.com>
2020-10-19 13:49:45 -0500

    dynamic paragraph using class names

--- src/App.js
+++ src/App.js
@@ -79,11 +79,21 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
+        const classes = [];
+        if (this.state.persons.length <= 2) {
+            classes.push('red'); //classes = ['red]
+        }
+        if (this.state.persons.length <= 1) {
+            classes.push('bold'); //classes =['bold','red']
+        }
+
+
+        
 
         return (
             <div className="App">
                 <h1>Hi, I am a React App</h1>
-                <p>This is really working!</p>
+                <p className={classes.join(' ')}>This is really working!</p>
                 <button
                     style={style}
                     onClick={() => this.togglePersonsHandler()}>Toggle Persons
