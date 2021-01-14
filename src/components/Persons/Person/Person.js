a69f5c2738fe030cb9c0089c97fa1f4229d2dc1e
Larry Castillo <larrycastillo2009@gmail.com>
2021-01-13 21:20:48 -0600

    converted person to class component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
 // const StyledDiv = styled.div`
@@ -13,16 +13,19 @@ import classes from './Person.css';
 //     width: 450px;
 // }
 // `
-const person = (props) => {
-    console.log('[Person.js] rendering...');
-    return (
-        // <div className="Person" style={style}>
-       <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
-};
+class Person extends Component {
 
-export default person;
+    render() {
+        console.log('[Person.js] rendering...');
+        return (
+            // <div className="Person" style={style}>
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
+}
+
+export default Person;
